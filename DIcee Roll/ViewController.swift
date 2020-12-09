481fabe4965548b2e39a023800885fbf52e57f9b
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2020-12-09 22:07:52 +0000

    First Commit
    Uploading Dicce Roll app

--- DIcee Roll/ViewController.swift
+++ DIcee Roll/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceeIndex1 : Int = 0
+    var randomDiceeIndex2 : Int = 0
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        updateDiceImage()
+        
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImage()
+    }
+    
+    func updateDiceImage() {
+        
+        randomDiceeIndex1 = Int(arc4random_uniform(6)+1)
+        randomDiceeIndex2 = Int(arc4random_uniform(6)+1)
+        
+        diceImageView1.image = UIImage(named: "dice\(randomDiceeIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceeIndex2)")
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImage()
+    }
 
 }
-
